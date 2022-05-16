8f8f5f5930124782ef029b571a10c1125417ab0e
EmanuelMTDF <104039755+EmanuelMTDF@users.noreply.github.com>
2022-05-16 00:10:51 -0300

    Actualizacion de datos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 // EJERCICIO "DIVISORES"
 
+/*
+• Implemente un método llamado cantidadDeDivisores que
+reciba un número entero y devuelva la cantidad de divisores
+• Por ejemplo, para el número 16, sus divisores son 1, 2, 4, 8,
+16, por lo que la respuesta debería ser 5
+
+COMENTARIO: NO COMPRENDO EXACTAMENTE A QUÉ HACE REFERENCIA EL SIGUIENTE INCISO:
+• Re-utilice el método esMultiplo implementado para el ejercicio
+anterior
+*/
+
 let numero: number = Number(prompt("Por favor, ingrese un número"));
 
 function cantidadDeDivisores(numero: number): number {
